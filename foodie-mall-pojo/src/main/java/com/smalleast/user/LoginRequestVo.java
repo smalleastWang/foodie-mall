48f81cc9f824748881645280c94f57fd4e678503
wangxiaodonga <wangxiaodonga@zbj.com>
2020-11-19 16:02:14 +0800

    update

---
+++ foodie-mall-pojo/src/main/java/com/smalleast/user/LoginRequestVo.java
@@ -0,0 +1,18 @@
+package com.smalleast.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Author : wangxiaodong
+ * @Program : com.smalleast.user
+ * @Description : TODO
+ * @Date 2020/11/19 下午3:42
+ **/
+
+@Getter
+@Setter
+public class LoginRequestVo {
+    private String username;
+    private String password;
+}
