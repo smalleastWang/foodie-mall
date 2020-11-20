57d5bbf478cccf811e4314d7bc00eb0cf5a3ea7f
wangxiaodonga <wangxiaodonga@zbj.com>
2020-11-20 17:16:02 +0800

    .

---
+++ foodie-mall-mapper/src/main/java/com/smalleast/Application.java
@@ -0,0 +1,11 @@
+package com.smalleast;
+
+/**
+ * @Author : wangxiaodong
+ * @Program : com.smalleast
+ * @Description : TODO
+ * @Date 2020/11/20 下午5:11
+ **/
+
+public class Application {
+}
---
+++ foodie-mall-service/src/main/java/com/smalleast/Application.java
@@ -0,0 +1,11 @@
+package com.smalleast;
+
+/**
+ * @Author : wangxiaodong
+ * @Program : com.smalleast
+ * @Description : TODO
+ * @Date 2020/11/20 下午5:12
+ **/
+
+public class Application {
+}
